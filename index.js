c17f495ffe802a0ddba60f4d001a19ee3e1ffa68
Antony Donovan <antony.donovan@generalassemb.ly>
2015-10-12 23:13:13 -0400

    Template for ajax lesson
    Focusing on getting config correct

--- index.js
+++ index.js
@@ -1,6 +1,166 @@
-//jQuery.ajax
-$(function(){
-  'use strict';
-//  var sa = '//localhost:3000';
-  var sa = 'https://young-citadel-2431.herokuapp.com'
+'use strict';
+var tttapi = {
+  gameWatcher: null,
+  ttt: 'http://ttt.wdibos.com',
+
+  ajax: function(config, cb) {
+    $.ajax(config).done(function(data, textStatus, jqxhr) {
+      cb(null, data);
+    }).fail(function(jqxhr, status, error) {
+      cb({jqxher: jqxhr, status: status, error: error});
+    });
+  },
+
+  register: function register(credentials, callback) {
+    this.ajax({
+    }, callback);
+  },
+
+  login: function login(credentials, callback) {
+    this.ajax({
+    }, callback);
+  },
+
+  //Authenticated api actions
+  listGames: function (token, callback) {
+    this.ajax({
+    }, callback);
+  },
+
+  createGame: function (token, callback) {
+    this.ajax({
+    }, callback);
+  },
+
+  showGame: function (id, token, callback) {
+    this.ajax({
+    }, callback);
+  },
+
+  joinGame: function (id, token, callback) {
+    this.ajax({
+    }, callback);
+  },
+
+  markCell: function (id, data, token, callback) {
+    this.ajax({
+    }, callback);
+  },
+
+  watchGame: function (id, token) {
+    var url = this.ttt + '/games/' + id + '/watch';
+    var auth = {
+      Authorization: 'Token token=' + token
+    };
+    this.gameWatcher = resourceWatcher(url, auth); //jshint ignore: line
+    return this.gameWatcher;
+  }
+};
+
+
+//$(document).ready(...
+$(function() {
+  var form2object = function(form) {
+    var data = {};
+    $(form).children().each(function(index, element) {
+      var type = $(this).attr('type');
+      if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+        data[$(this).attr('name')] = $(this).val();
+      }
+    });
+    return data;
+  };
+
+  var wrap = function wrap(root, formData) {
+    var wrapper = {};
+    wrapper[root] = formData;
+    return wrapper;
+  };
+
+  var callback = function callback(error, data) {
+    if (error) {
+      console.error(error);
+      $('#result').val('status: ' + error.status + ', error: ' +error.error);
+      return;
+    }
+    $('#result').val(JSON.stringify(data, null, 4));
+  };
+
+  $('#register').on('submit', function(e) {
+    var credentials = wrap('credentials', form2object(this));
+    tttapi.register(credentials, callback);
+    e.preventDefault();
+  });
+
+  $('#login').on('submit', function(e) {
+    var credentials = wrap('credentials', form2object(this));
+    var cb = function cb(error, data) {
+      if (error) {
+        callback(error);
+        return;
+      }
+      callback(null, data);
+      $('.token').val(data.user.token);
+    };
+    e.preventDefault();
+    tttapi.login(credentials, cb);
+  });
+
+  $('#list-games').on('submit', function(e) {
+    var token = $(this).children('[name="token"]').val();
+    e.preventDefault();
+    tttapi.listGames(token, callback);
+  });
+
+  $('#create-game').on('submit', function(e) {
+    var token = $(this).children('[name="token"]').val();
+    e.preventDefault();
+    tttapi.createGame(token, callback);
+  });
+
+  $('#show-game').on('submit', function(e) {
+    var token = $(this).children('[name="token"]').val();
+    var id = $('#show-id').val();
+    e.preventDefault();
+    tttapi.showGame(id, token, callback);
+  });
+
+  $('#join-game').on('submit', function(e) {
+    var token = $(this).children('[name="token"]').val();
+    var id = $('#join-id').val();
+    e.preventDefault();
+    tttapi.joinGame(id, token, callback);
+  });
+
+  $('#mark-cell').on('submit', function(e) {
+    var token = $(this).children('[name="token"]').val();
+    var id = $('#mark-id').val();
+    var data = wrap('game', wrap('cell', form2object(this)));
+    e.preventDefault();
+    tttapi.markCell(id, data, token, callback);
+  });
+
+  $('#watch-game').on('submit', function(e){
+    var token = $(this).children('[name="token"]').val();
+    var id = $('#watch-id').val();
+    e.preventDefault();
+
+    var gameWatcher = tttapi.watchGame(id, token);
+
+    gameWatcher.on('change', function(data){
+      var parsedData = JSON.parse(data);
+      if (data.timeout) { //not an error
+        this.gameWatcher.close();
+        return console.warn(data.timeout);
+      }
+      var gameData = parsedData.game;
+      var cell = gameData.cell;
+      $('#watch-index').val(cell.index);
+      $('#watch-value').val(cell.value);
+    });
+    gameWatcher.on('error', function(e){
+      console.error('an error has occured with the stream', e);
+    });
+  });
+
 });
